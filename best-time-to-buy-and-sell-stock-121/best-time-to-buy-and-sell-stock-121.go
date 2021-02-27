7ab9e973d73a85ba9af847d1120eb74d437ccce9
sk <senkai0903@vip.qq.com>
2021-02-28 00:43:14 +0800

    ac 121

---
+++ best-time-to-buy-and-sell-stock-121/best-time-to-buy-and-sell-stock-121.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func maxProfit(prices []int) int {
+	dp_i_0 := 0
+	dp_i_1 := math.MinInt8
+	for _, p := range prices {
+		dp_i_0 = max(dp_i_0, dp_i_1+p)
+		dp_i_1 = max(dp_i_1, -p)
+	}
+	return dp_i_0
+}
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
